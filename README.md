95761e347ef1129b70bf25896f1ad351d773f748
LuckyLuuk035 <LuckyLuuk035@users.noreply.github.com>
2021-03-18 13:29:45 +0100

    first edit

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 We gaan aan het werk met het opzetten van een rule-based systeem ten behoeve van de recommendation engine.
 
 Voor de Content Filtering heb ik bedacht dat:
-Als mensen naar een bepaald product kijken ze ook geinterresseerd kunnen zijn in een ander product uit de zelfde categorie met de zelfde doelgroep en target.
+Als mensen naar een bepaald product kijken ze ook geinterresseerd kunnen zijn in een ander product uit de zelfde categorie met de zelfde doelgroep (en target).
+Waar deze eigenschappen overeen komen worden ze samen gevoegd als 1 categorie waar alle producten met de zelfde eigenschappen ook aan worden toegevoegd.
 
 Voor de Collaborative Filtering kijken we naar de users die...
--- Rules/Content_rules.py
+++ Rules/Content_rules.py
@@ -1,3 +1,9 @@
+import psycopg2
+import pandas as pd
+
+from _functions.config import config
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
 """
 pseudocode for content filtering
 
@@ -17,5 +23,15 @@ pseudocode for content filtering
 """
 
 def contentFilter():
-
+    # Pas deze regel aan om de database aan te passen
+    commands = ["CREATE TABLE contentFilter AS SELECT idproducts, category, doelgroep, target FROM products"]
+    db = config()
+    con = psycopg2.connect(**db)
+    cursor = con.cursor()
+    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    for command in commands:
+        cursor.execute(command)
+        con.commit()
+    print('Done')
+    con.close()
     return
