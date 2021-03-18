aaabaa3de7ced2825b6b3e818376c4aa7f45df2f
LuckyLuuk035 <LuckyLuuk035@users.noreply.github.com>
2021-03-18 14:11:16 +0100

    Update Content_rules.py

--- Rules/Content_rules.py
+++ Rules/Content_rules.py
@@ -4,23 +4,6 @@ import pandas as pd
 from _functions.config import config
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
-"""
-pseudocode for content filtering
-
-1. Categorys gaan pakken
-2. Orders/events pakken 
-2a/ populaire producten pakken
-2b/ genders pakken
-2c/ prijzen pakken
-2d/ brand pakken
-2e/ product name pakken
-2f/ variant pakken?
-2g/ aanbieding pakken/folder actief
-3. query analyseren
-4. visitors en products aan linken in koppeltabel similars
-5. recommendations geven
-
-"""
 
 def contentFilter():
     # Pas deze regel aan om de database aan te passen
@@ -34,6 +17,5 @@ def contentFilter():
     for command in commands:
         cursor.execute(command)
         con.commit()
-    print('Done')
     con.close()
     return
